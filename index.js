7919bfcc32b1310311b534079dabec2532a06866
Martin Koidu <martin.koidu@pipedrive.com>
2020-11-11 21:29:05 +0200

    fetch portfolio information from a separate page

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const browserHelper = require('./browser-helper');
 const COMPANY_ACCOUNT_SELECTOR =
   'body > div.modal.modal-list > div > div > div.modal-body.cards.text-left > div:nth-child(4)';
 const TOTAL_PORTFOLIO_VALUE_SELECTOR =
-  'div.wallet-table__col.grid__col-2-4.text--semibold.flex-justify--end.th-investments-total';
+  '#js-main-view > section > div.box-fill--gray.padding-around--m > div > div:nth-child(10)';
 const LAST_PRICE_PORTFOLIO_VALUE_SELECTOR =
-  'div.wallet-table__col.grid__col-4-6.text--semibold.flex-justify--end.th-investments-value';
+  '#js-main-view > section > div.box-fill--gray.padding-around--m > div > div:nth-child(12)';
 
 const getMonetaryValue = (value) => value.replace(/EUR|,|\s/g, '');
 
@@ -18,7 +18,11 @@ const getMonetaryValue = (value) => value.replace(/EUR|,|\s/g, '');
     await pageHelper.login(page);
     await page.waitForSelector('.modal-dialog');
     await page.click(COMPANY_ACCOUNT_SELECTOR);
-    await page.waitForSelector('.wallet--assets');
+    console.log('selected compant');
+    await page.waitForSelector('.assets-block');
+    console.log('assets block found');
+    await page.goto('https://www.funderbeam.com/portfolio');
+    console.log('navigated to portfolio page');
     await page.waitForSelector(TOTAL_PORTFOLIO_VALUE_SELECTOR);
 
     const portfolioValue = getMonetaryValue(
@@ -31,6 +35,12 @@ const getMonetaryValue = (value) => value.replace(/EUR|,|\s/g, '');
       ),
     );
     await browser.close();
+
+    console.log(JSON.stringify({
+      value: lastPricePortfolioValue,
+      initial_investment: portfolioValue,
+      profit: lastPricePortfolioValue - portfolioValue,
+    }));
 
     const connection = await db.getConnection();
     await db.insertPortfolioValue(connection, {
